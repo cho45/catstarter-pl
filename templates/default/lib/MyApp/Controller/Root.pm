07f3e82e37a5c569e8ac82ecd1b1f1548ced7e30
cho45 <cho45@lowreal.net>
2008-04-16 18:02:11 +0900

    MY OWN Root.pm

--- templates/default/lib/MyApp/Controller/Root.pm
+++ templates/default/lib/MyApp/Controller/Root.pm
@@ -4,52 +4,25 @@ use strict;
 use warnings;
 use base 'Catalyst::Controller';
 
-#
-# Sets the actions in this controller to be registered with no prefix
-# so they function identically to actions created in MyApp.pm
-#
 __PACKAGE__->config->{namespace} = '';
 
-=head1 NAME
-
-MyApp::Controller::Root - Root Controller for MyApp
-
-=head1 DESCRIPTION
-
-[enter your description here]
-
-=head1 METHODS
-
-=cut
-
-=head2 default
-
-=cut
-
 sub default : Private {
-    my ( $self, $c ) = @_;
+	my ( $self, $c ) = @_;
 
-    # Hello World
-    $c->response->body( $c->welcome_message );
+	# Hello World
+	$c->response->body( $c->welcome_message );
 }
 
-=head2 end
+sub end : ActionClass('RenderView') {
+	my ( $self, $c ) = @_;
 
-Attempt to render a view, if needed.
+	$c->res->header( 'Cache-Control' => 'private' );
+	$c->stash->{class} = [ split '/', $c->action ]->[0];
 
-=cut 
-
-sub end : ActionClass('RenderView') {}
-
-=head1 AUTHOR
-
-さとう
-
-=head1 LICENSE
-
-This library is free software, you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
+	if (!$ENV{CATALYST_DEBUG} && scalar(@{ $c->error }) ) {
+		$c->stash->{template} = 'errors/500.pl.html';
+		$c->error(0);
+	}
+}
 
 1;
