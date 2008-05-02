49c28f63d1306f9590f651356c2c0a2bcf32b30a
cho45 <cho45@lowreal.net>
2008-05-02 11:33:19 +0900

    Comply the rule.

--- templates/default/lib/MyApp/Controller/Root.pm
+++ templates/default/lib/MyApp/Controller/Root.pm
@@ -7,18 +7,18 @@ use base 'Catalyst::Controller';
 __PACKAGE__->config->{namespace} = '';
 
 sub default : Private {
-	my ( $self, $c ) = @_;
+	my ($self, $c) = @_;
 
 	$c->res->status(404);
 	$c->stash->{template} = 'errors/404.pl.html';
 }
 
 sub index : Private {
-	my ( $self, $c ) = @_;
+	my ($self, $c) = @_;
 }
 
 sub end : ActionClass('RenderView') {
-	my ( $self, $c ) = @_;
+	my ($self, $c) = @_;
 
 	$c->res->header( 'Cache-Control' => 'private' );
 	$c->stash->{class} = [ split '/', $c->action ]->[0];
--- templates/default/lib/MyApp/View/HTML.pm
+++ templates/default/lib/MyApp/View/HTML.pm
@@ -20,26 +20,4 @@ sub _render {
 	$ret;
 }
 
-=head1 NAME
-
-MyApp::View::HTML - MicroMason View Component
-
-=head1 SYNOPSIS
-
-In your end action:
-
-    $c->forward('MyApp::View::HTML');
-
-=head1 DESCRIPTION
-
-A description of how to use your view, if you're deviating from the
-default behavior.
-
-=head1 AUTHOR
-
-
-=head1 LICENSE
-
-=cut
-
 1;
